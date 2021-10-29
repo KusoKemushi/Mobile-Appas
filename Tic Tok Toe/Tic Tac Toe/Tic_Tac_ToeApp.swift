4087ab37c933bfc11372f789371a4be2018379fc
Eis <90634963+KusoKemushi@users.noreply.github.com>
2021-10-29 10:53:32 -0500

    Add files via upload

---
+++ Tic Tok Toe/Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Student on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
